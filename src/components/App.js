65cb308f61905f35538450b612e2ecc1f0ad4e35
Phylis Chepchumba <chumba.phyl@gmail.com>
2020-09-22 23:41:10 +0300

    render button pannel and display components

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+/* eslint-disable no-unused-vars */
+import React from 'react';
+import PropTypes from 'prop-types';
+import ButtonPanel from './ButtonPanel';
+import Display from './Display';
+
+export default function App() {
+  return (
+    <div>
+      <Display />
+      <ButtonPanel />
+    </div>
+  );
+}
