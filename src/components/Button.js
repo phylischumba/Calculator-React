7c67f63388adf2d555db90debbb0094e3cc12827
Phylis Chepchumba <chumba.phyl@gmail.com>
2020-09-22 23:42:39 +0300

    create button props and render calculator buttons

---
+++ src/components/Button.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+export default function Button({ name }) {
+  return (
+    <div>
+      {name}
+    </div>
+  );
+}
+
+Button.propTypes = {
+  name: PropTypes.string.isRequired,
+};
