19aa104a7567d449bd373a6afbf08f34dfef8c74
Phylis Chepchumba <chumba.phyl@gmail.com>
2020-09-25 17:12:54 +0300

    create the operate function that has calc operation logic

---
+++ src/logic/operate.js
@@ -0,0 +1,20 @@
+/* eslint-disable no-unused-vars */
+import Big from 'big.js';
+
+export default function operate(numberOne, numberTwo, operation) {
+  let result = 0;
+  const firstNumber = Big(numberOne);
+  const secondNumber = Big(numberTwo);
+
+  if (operation === '+') {
+    result = firstNumber.plus(secondNumber);
+  } else if (operation === '-') {
+    result = firstNumber.minus(secondNumber);
+  } else if (operation === 'x') {
+    result = firstNumber.times(secondNumber);
+  } else if (operation === '÷') {
+    result = firstNumber.div(secondNumber);
+  } else if (operation === '%') {
+    result = firstNumber.mod(secondNumber);
+  }
+}
