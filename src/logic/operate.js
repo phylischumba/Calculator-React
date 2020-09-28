c12d81f72ba732f919240890c255961061b5cdf0
Phylis Chepchumba <chumba.phyl@gmail.com>
2020-09-29 00:28:26 +0300

    return result as a string

--- src/logic/operate.js
+++ src/logic/operate.js
@@ -1,35 +1,31 @@
 import Big from 'big.js';
 
 const operate = (numberOne, numberTwo, operation) => {
-  let result;
+  let result = 0;
   const firstNumber = Big(numberOne || '0');
   const secondNumber = Big(numberTwo || '0');
 
   if (operation === '+') {
-    result = firstNumber.plus(secondNumber);
+    result = firstNumber.plus(secondNumber).toFixed();
   }
 
   if (operation === '-') {
-    result = firstNumber.minus(secondNumber);
+    result = firstNumber.minus(secondNumber).toFixed();
   }
 
   if (operation === 'X') {
-    result = firstNumber.times(secondNumber);
+    result = firstNumber.times(secondNumber).toFixed();
   }
 
   if (operation === '÷') {
-    // try {
-    result = firstNumber.div(secondNumber);
-    // } catch (err) {
-    //   result = 'ERROR';
-    // }
+    result = firstNumber.div(secondNumber).toFixed();
   }
 
   if (operation === '%') {
-    result = firstNumber.mod(secondNumber);
+    result = firstNumber.div(Big(100)).toFixed();
   }
 
-  return result.toString();
+  return result;
 };
 
 export default operate;
